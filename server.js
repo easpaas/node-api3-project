22c1c8cfc2d6d9dbe6c4e9b1aa25a366f6cfe2f4
evan <aspaasevan@gmail.com>
2020-05-06 12:14:13 -0700

    init commit

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {}
+function validateUserId(req, res, next) {}
+function validateUser(req, res, next) {}
+function validatePost(req, res, next) {}
 
 module.exports = server;
