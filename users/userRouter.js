f905fc637cf588190d3be2f7fbe474d115a6fc04
evan <aspaasevan@gmail.com>
2020-05-06 16:54:02 -0700

    validateUser and validateUserById middlewares complete

--- users/userRouter.js
+++ users/userRouter.js
@@ -4,7 +4,7 @@ const postDB = require('../posts/postDb.js');
 const router = express.Router();
 
 // Return user added to db successfully
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
   const user = req.body;
   userDB.insert(user)
     .then(user => {
@@ -12,12 +12,12 @@ router.post('/', (req, res) => {
     })
     .catch(error => {
       console.log(error)
-      res.status(500).json({ errorMessage: 'Could not connect to server.' })
+      res.status(500).json({ errorMessage: 'Could not add user.' })
     });
   });
 
 // Return post for user from valid id parameter
-router.post('/:id/posts', (req, res) => {
+router.post('/:id/posts', validateUserId, (req, res) => {
   const post = { ...req.body, user_id: req.params.id };
   postDB.insert(post)
     .then(post => {
@@ -42,7 +42,7 @@ router.get('/', (req, res) => {
 });
 
 // Return user from valid id parameter
-router.get('/:id', (req, res) => {
+router.get('/:id', validateUserId, (req, res) => {
   const userId = req.params.id;
   userDB.getById(userId)
     .then(user => {
@@ -51,12 +51,12 @@ router.get('/:id', (req, res) => {
     })
     .catch(error => {
       console.log(error);
-      res.status(500).json({ errorMessage: 'Error connecting to server' })
+      res.status(500).json({ errorMessage: `Error retrieving user with id ${userId}` })
     })
 });
 
 // Return all posts of user from valid id parameter
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
   const userId = req.params.id;
   userDB.getUserPosts(userId)
     .then(posts => {
@@ -69,7 +69,7 @@ router.get('/:id/posts', (req, res) => {
 });
 
 // Delete user from valid id parameter
-router.delete('/:id', (req, res) => {
+router.delete('/:id', validateUserId, (req, res) => {
   const userId = req.params.id;
   userDB.remove(userId)
     .then(success => {
@@ -82,7 +82,7 @@ router.delete('/:id', (req, res) => {
 });
 
 // Return success after user is updated with db
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUserId, (req, res) => {
   userDB.update(req.params.id, req.body) 
     .then(success => {
       console.log(success);
@@ -98,10 +98,27 @@ router.put('/:id', (req, res) => {
 
 function validateUserId(req, res, next) {
   // do your magic!
+  const userId = req.params.id;
+  userDB.getById(userId)
+    .then(success => {
+      res.status(201).json(success)
+    })
+    .catch(error => {
+      res.status(404).json({ errorMessage: `Could not find user with id ${userId}` });
+    });
+  
+  next();
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if (!req.body) {
+    res.status(400).json({ message: 'Missing user data.' })
+  } else if (!req.body.name) {
+    res.status(400).json({ message: 'Missing required name field.' })
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
