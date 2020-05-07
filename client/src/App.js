d5b6fa6086217eec184d88bc9ea398da3c1e2fa2
evan <aspaasevan@gmail.com>
2020-05-07 07:11:09 -0700

    client sends and recieves user data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
 
-  
+  useEffect(() => {
+    axios.get('http://localhost:4000/api/users')
+      .then(response => {
+        console.log(response.data)
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }, []);
+
   return (
     <div className="App">
-    
+      <h1>Posts from Lord of The Rings Characters</h1>
+      <div className="post-container">
+        {
+          users.map(user => {
+            return (
+              <div key={user.id} className="post">
+                <h2>{`User: ${user.name}`}</h2>
+                <p>{`User Identification: ${user.id}`}</p>
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
